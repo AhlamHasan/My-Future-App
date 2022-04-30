81c07d32517b62621d0a29761f54153a2f819a6c
Wesayef Aldosary <wesayef88@yahoo.com>
2022-04-30 21:46:44 +0300

    Connect To Firestore and create Users Collection, solve all sign in & sign up issues.

--- app/src/main/java/com/example/myfuture/SignInActivity.java
+++ app/src/main/java/com/example/myfuture/SignInActivity.java
@@ -1,23 +1,35 @@
 package com.example.myfuture;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.datepicker.MaterialTextInputPicker;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class SignInActivity extends AppCompatActivity {
 
+
     TextInputLayout Email, Password;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String strEmail,strPassword,TAG="null";
 
 
 
@@ -39,23 +51,44 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void callHomeScreen(View view) {
-       if(!validateEmail() | !validatePassword()){
-           return;
-       }
+        if(!validateEmail() | !validatePassword()){
+            return;
+        }
 
-        Intent i=new Intent(SignInActivity.this,HomeActivity.class);
-        startActivity(i);
+        db.collection("Users").whereEqualTo("Email", strEmail).whereEqualTo("Password", strPassword).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(queryDocumentSnapshots.isEmpty()){
+                    Email.setError("Email is not found");
+                    Password.setError("or password is not correct");
+                    Log.d(TAG, "This account not exist");
+                }else {
+                    List<DocumentSnapshot> DocList= queryDocumentSnapshots.getDocuments();
+                    for(DocumentSnapshot snapshot: DocList){
+                        Signup.UID= snapshot.getId();
+                        Log.d(TAG, "DocumentSnapshot is found with ID: " + Signup.UID);
+                    }
 
+                    Intent i=new Intent(SignInActivity.this,HomeActivity.class);
+                    startActivity(i);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, " onFailure "+ e);
+            }
+        });
 
     }
 
     private boolean validateEmail(){
-        String val=Email.getEditText().getText().toString().trim();
+        strEmail=Email.getEditText().getText().toString().trim();
         String checkEmail ="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-        if(val.isEmpty()){
+        if(strEmail.isEmpty()){
             Email.setError("Field can not be empty");
             return false;
-        }else if(!val.matches((checkEmail))){
+        }else if(!strEmail.matches((checkEmail))){
             Email.setError("Invalid Email!");
             return false;
         }else {
@@ -66,20 +99,21 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private boolean validatePassword(){
-        String val=Password.getEditText().getText().toString().trim();
+        strPassword =Password.getEditText().getText().toString().trim();
         String checkPassword = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
 
-        if(val.isEmpty()){
+        if(strPassword.isEmpty()){
             Password.setError("Field can not be empty");
             return false;
-        } else if(!val.matches((checkPassword))){
+        } else if(!strPassword.matches((checkPassword))){
             Password.setError("Password should contain 8 characters,at least 1 digit, 1 special character");
             return false;
         } else {
             Password.setError(null);
             Password.setErrorEnabled(false);
             return true;
-             }
+        }
     }
+
 
 }
--- app/src/main/java/com/example/myfuture/Signup.java
+++ app/src/main/java/com/example/myfuture/Signup.java
@@ -10,11 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -52,30 +55,25 @@ public class Signup extends AppCompatActivity {
             return;
         }
 
-        Map<String, Object> dataToSave = new HashMap<>();
-        dataToSave.put("First Name", First_name);
-        dataToSave.put("Last Name",Last_name);
-        dataToSave.put("Email", strEmail);
-        dataToSave.put("Password", strPassword);
-        dataToSave.put("Point", 0);
-        String id = db.collection("collection_name").document().getId();
-        System.out.println(id);
-        db.collection("Users").add(dataToSave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("Users").whereEqualTo("Email", strEmail).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
-                UID=documentReference.getId();
-            }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if(task.getResult().isEmpty()){
+                        SaveUser();
+                    }else{
+                        Email.setError("Email account is exist ");
+                        Log.d(TAG, "Email account is already exist ");
                     }
-                });
 
-        Intent i=new Intent(Signup.this,HomeActivity.class);
-        startActivity(i);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, " onFailure "+ e);
+            }
+        });
     }
 
     private boolean validateFirstName(){
@@ -137,5 +135,34 @@ public class Signup extends AppCompatActivity {
             Password.setErrorEnabled(false);
             return true;
         }
+    }
+
+    private void SaveUser(){
+
+        Map<String, Object> dataToSave = new HashMap<>();
+        dataToSave.put("First Name", First_name);
+        dataToSave.put("Last Name", Last_name);
+        dataToSave.put("Email", strEmail);
+        dataToSave.put("Password", strPassword);
+        dataToSave.put("Point", 0);
+        String id = db.collection("collection_name").document().getId();
+        System.out.println(id);
+        db.collection("Users").add(dataToSave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                UID = documentReference.getId();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+
+        Intent i = new Intent(Signup.this, HomeActivity.class);
+        startActivity(i);
+
+
     }
 }
