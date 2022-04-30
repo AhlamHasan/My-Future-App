780bc08ca9ef7ee8235e2d854616d9e67656d29d
Noufalotaibi <74366428+Noufalotaibi3@users.noreply.github.com>
2022-04-30 23:04:27 +0300

    Update eduModel.java

--- app/src/main/java/com/example/myfuture/Model/eduModel.java
+++ app/src/main/java/com/example/myfuture/Model/eduModel.java
@@ -2,14 +2,14 @@ package com.example.myfuture.Model;
 
 public class eduModel extends eduID {
 
-    private String degree, uni, grade, year;
+    private String degree, degree2, grade, year;
 
     public String getDegree() {
         return degree;
     }
 
-    public String getUni() {
-        return uni;
+    public String getDegree2() {
+        return degree2;
     }
 
     public String getGrade() {
